afc5a65a9fe136311a50d2453e0b047ea083e1b3
Bartlomiej Grzadziel <bartlomiejgrzadziel2@gmail.com>
2025-10-10 10:49:44 +0200

    better password control

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -8,7 +8,7 @@ import { PasswordModule } from 'primeng/password';
 import { ButtonModule } from 'primeng/button';
 import { MessageModule } from 'primeng/message';
 import { RippleModule } from 'primeng/ripple';
-
+import { ToastrService } from 'ngx-toastr';
 import { AuthService } from '../../../services/auth/auth.service';
 import { Utils } from '../../../utils/utils';
 
@@ -24,9 +24,11 @@ export class Register {
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private authService = inject(AuthService);
+  private toastr = inject(ToastrService);
 
   loading = signal(false);
   error = signal<string | null>(null);
+  checkPassword = signal<string | null>(null);
 
   form = this.fb.group({
     username: ['', Validators.required],
@@ -39,24 +41,28 @@ export class Register {
     this.loading.set(true);
 
     const { username, password } = this.form.getRawValue();
-
     this.authService
       .register({ username: username!, password: password! })
       .subscribe({
         next: (res) => {
           this.loading.set(false);
-          // res = { ok: true, message: 'User registered successfully' }
           if (res?.ok) {
-            // Optionnel: afficher le message puis rediriger
             this.router.navigateByUrl('/login');
           } else {
             this.error.set('Registration failed');
           }
         },
-        error: (e) => {
-          this.loading.set(false);
-          this.error.set(Utils.getErrorOrDefault(e,'Registration failed' ));
-        },
-      });
+      error: (e) => {
+        this.loading.set(false);
+        if (e.status === 400 && e.error?.errors) {
+          const errors = e.error.errors;
+        if (errors.password) {
+          this.checkPassword.set(errors.password);
+        }
+        } else {
+          this.error.set('Registration failed');
+        }
+      }
+    });
   }
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -42,7 +42,7 @@ export class AuthService {
     return this.http
       .post(`${this.apiBase}${API_ROUTES.auth.register}`, request, {
         observe: 'response',
-        responseType: 'text',
+        responseType: 'json',
       })
       .pipe(
         map((res) => {
