fee29140e43c1130a7b2a81f25139fda946eb66c
Quentin WESSER <qwesser@consultants-solutec.fr>
2025-10-14 14:43:41 +0200

    Fix dashboard

--- src/app/utils/pipe/percentageClassPipe.ts
+++ src/app/utils/pipe/percentageClassPipe.ts
@@ -4,9 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'percentageColor'
 })
 export class PercentageColorPipe implements PipeTransform {
-  transform(value: number | null | undefined | string): string {
+  transform(value: number | null | undefined | string, zeroBased: boolean = false): string {
     if (typeof value == 'string') return '';
     if (!value) return '';
+    if (zeroBased && value >= 0) return 'green';
     if (value >= 1) return 'green';
     return 'red'
   }
